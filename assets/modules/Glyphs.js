504b3b9a0533b423f83baca0da86ecf3b03b5870
Deshan Nawanjana <deshan.uok@gmail.com>
2026-06-12 11:38:38 +0530

    Glyphs find Method

--- assets/modules/Glyphs.js
+++ assets/modules/Glyphs.js
@@ -21,6 +21,26 @@ export class Glyphs {
     this.data = data
   }
   /**
+   * Returns set of icon results for query string
+   * @param {string} query Search query
+   * @returns {{ id: string, score: number }[]}
+   */
+  find(query) {
+    // get all result items
+    const items = this.meta.icons.map(id => ({ id, score: 1 }))
+    // return all items for empty query
+    if (query.trim() === "") return items
+    // split into query keywords
+    const keywords = query.toLowerCase().split(" ").filter(word => word !== "")
+    // get scored results by keywords
+    const results = items.map(item => ({
+      // calculate score by available word count in icon id
+      id: item.id, score: keywords.filter(word => item.id.includes(word)).length
+    }))
+    // return filtered and sorted results
+    return results.filter(item => item.score > 0).sort((a, b) => b.score - a.score)
+  }
+  /**
    * Generates SVG element for an icon
    * @param {string} id Identifier of the icon
    * @returns {SVGSVGElement | null}
