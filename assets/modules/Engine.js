5a2f97f45aef7f509b10f85facfc607aced6d9d8
Deshan Nawanjana <deshan.uok@gmail.com>
2026-06-14 13:35:05 +0530

    Engine find Method

--- assets/modules/Engine.js
+++ assets/modules/Engine.js
@@ -1,4 +1,5 @@
 import { Glyphs } from "./Glyphs.js"
+import { Icon } from "./Icon.js"
 
 export class Engine {
   /**
@@ -7,16 +8,52 @@ export class Engine {
   constructor(baseURL) {
     /** @type {string} Objects base URL */
     this.baseURL = baseURL
-    /** @type {Object.<string, import("./Glyphs").Meta> | null} Collections listing */
-    this.data = null
-    /** @type {Glyphs[]} Icon data listing */
-    this.list = []
+    /** @type {Object.<string, import("./Glyphs.js").Meta>} Icon meta data mapping */
+    this.data = {}
+    /** @type {Object.<string, Glyphs>} Glyphs mapping */
+    this.list = {}
   }
   /** Loads collections listing */
   async load() {
-    // fetch listing data
+    // fetch collections
     const resp = await fetch(`${this.baseURL}/data.json`)
-    // store parsed response
+    // get parsed response
     this.data = await resp.json()
+    // for each collection
+    for (const name of Object.keys(this.data)) {
+      // create glyphs collection
+      this.list[name] = new Glyphs(name, this.data[name], null)
+    }
+  }
+  /**
+   * Returns set of icon results for query string
+   * @param {string} query Search query
+   * @param {string[] | null} groups Selected collections
+   * @returns {{ id: string, collection: Glyphs, score: number }[]}
+   */
+  find(query, groups) {
+    // split query into keywords
+    const keywords = query.toLowerCase().split(" ").filter(word => word.length > 0)
+    // output array
+    const output = []
+    // for each meta item
+    for (const name of Object.keys(this.data)) {
+      // continue if not selected
+      if (groups && !groups.includes(name)) continue
+      // for each icon in collection
+      for (const id of this.data[name].icons) {
+        // calculate score levels
+        const highScore = keywords.filter(word => id.includes(word)).length
+        const lowScore = keywords.filter(word => id.split("-").includes(word)).length * 10
+        // calculate final score
+        const score = highScore + lowScore
+        // continue if no score
+        if (keywords.length > 0 && score === 0) continue
+        // push to results
+        output.push({ id, collection: this.list[name], score })
+      }
+    }
+    // return sorted output output
+    return output.sort((a, b) => b.score - a.score)
   }
 }
